102addefb752814b08e53b928d3b089beb12feed
Sheena Mohsin <sheenamohsin123@gmail.com>
2024-04-08 03:53:57 +0500

    number guessing game

---
+++ index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+const randomnumb = Math.floor(Math.random())*6 + 1
+const answer = await inquirer.prompt([
+{
+    name:"userGuessedNumber",
+    type: "number",
+    message: "please guess a number between 1-10"
+}
+]);
+if (answer.userGuessedNumber === randomnumb){
+    console.log ("Congratulations you guessed right number!")
+} else {
+    console.log("You guessed wrong number! ")
+};
